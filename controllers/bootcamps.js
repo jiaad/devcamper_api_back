7a14ea1a90163cd758e4d443e2bdf2a7725b44da
jiaad <abdulziad08@outlook.com>
2020-02-25 21:11:52 +0100

    Controller: added first ever controller && and used router.route('/') method

---
+++ controllers/bootcamps.js
@@ -0,0 +1,35 @@
+// @desc      Get all bootcamps
+// @route     GET /api/v1/bootcamps
+// @access    Public
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({succes: true, msg: "show all bootcamps", hello: req.hello})
+}
+
+// @desc      Get single bootcamps
+// @route     GET /api/v1/bootcamps/:id
+// @access    Public
+exports.getBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `show bootcamp ${req.params.id}` })
+}
+
+// @desc      Create new bootcamp
+// @route     POST /api/v1/bootcamps
+// @access    Private
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "create new bootcamp" })
+
+}
+
+// @desc      Update bootcamp
+// @route     put /api/v1/bootcamps/:id
+// @access    Private
+exports.updateBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `update bootcamp ${req.params.id}` })
+}
+
+// @desc      Delete bootcamp
+// @route     DELETE /api/v1/bootcamps/:id
+// @access    Private
+exports.deleteBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `delete bootcamp ${req.params.id}` })
+}
