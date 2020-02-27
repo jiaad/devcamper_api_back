9ac313c67cc010c3a5331330a4a16da34e4cfee2
jiaad <abdulziad08@outlook.com>
2020-02-27 13:09:17 +0100

    APP FILE: Package + configuration

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const logger = require('./middleware/logger')
-const morgan = require('morgan')
+const express   = require('express')
+const dotenv    = require('dotenv')
+const logger    = require('./middleware/logger')
+const morgan    = require('morgan')
+const color     = require('colors')
+const errorHandler    = require('./middleware/error')
 
 //LOAD ENV VARS
 // IMPORTANT: toujours mettre le ENV au dessus des import qui ont des ENV
@@ -17,13 +19,13 @@ connectDB()
 const app = express()
 const PORT = process.env.PORT || 5000
 
+// Body Parser
+app.use(express.json());
 
 //app.use(logger)
 // Dev loggin middleware
 if(process.env.NODE_ENV == 'development'){
-  app.use(morgan('dev',{
-    skip: function(req, res) { return res  }
-  }));
+  app.use(morgan('dev'))
 }
 
 // ROUTES FILES
@@ -34,18 +36,16 @@ const bootcamps = require('./routes/bootcamps')
 app.use('/api/v1/bootcamps', bootcamps);
 
 
+// ERROR HANLDER : MUST BE AFTER MOUNT ROUTERS
+app.use(errorHandler)
 
 // SERVER INITIALIZATION
 const server = app.listen(PORT,
-  console.log(`server running in :
-  ${process.env.NODE_ENV} ||
-  on port : ${PORT}`))
+  console.log(`server running in :${process.env.NODE_ENV} || on port : ${PORT}`.yellow.bold))
 
 
 // Handle Unhandle promise rejection
 
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`ERROR: ${err.message}`);
-    //
-    server.close(() => process.exit(1));
+  console.log(`ERROR: ${err.message}`.red)
 })
