2df44d7cf2f43effefd7705918e1b08a6b5f4d00
jiaad <abdulziad08@outlook.com>
2020-02-27 13:08:35 +0100

    Config: added colors on DB console.log to find it easily on the teminal

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true, useUnifiedTopology: true
   });
-  console.log(`MongoDB Connected: ${conn.connection.host}`)
+  console.log(`MongoDB Connected: ${conn.connection.host}`.blue.underline.bold)
 }
 
 
