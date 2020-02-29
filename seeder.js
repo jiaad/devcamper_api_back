19f31229fd4777a6e78391c876034f4ab9e10000
jiaad <abdulziad08@outlook.com>
2020-02-29 17:42:38 +0100

    Course Model: added course model and seed for it + added relationship between course and model: every course belongs_to a model Bootcamp

--- seeder.js
+++ seeder.js
@@ -12,6 +12,7 @@ dotenv.config({path: './config/config.env'})
 // Load models
 
 const Bootcamp = require('./models/Bootcamp')
+const Course = require('./models/Course')
 
 
 // Connect to DB
@@ -21,14 +22,19 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 
 // Read JSON files
 
-const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'))
-
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+)
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+)
 
 // import Into DB
 
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps)
+    await Course.create(courses)
     console.log('Data Imported...'.green.inverse);
     process.exit()
   } catch (err) {
@@ -42,6 +48,7 @@ const importData = async () => {
 const deleteData = async() => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log("Data deleted....".red.inverse)
     process.exit()
   } catch (e) {
