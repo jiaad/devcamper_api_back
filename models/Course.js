51e1926094281bf1d6177165958ef2f0549ef2c5
jiaad <abdulziad08@outlook.com>
2020-02-29 20:45:16 +0100

    Error: aggregate function always return null

--- models/Course.js
+++ models/Course.js
@@ -32,12 +32,38 @@ const CourseSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     },
-    bootcamp:{
+    bootcamp: {
         type: mongoose.Schema.ObjectId,
         ref: 'Bootcamp',
         required: true
-    }
+      }
      
 });
+
+CourseSchema.statics.getAverageCost = async function(bootcampId) {
+    const obj = await this.aggregate([
+      {
+        $match: { bootcamp: bootcampId }
+      },
+      {
+        $group: {
+          _id: '$bootcamp',
+          averageCost: { $avg: '$tuition' }
+        }
+      }
+    ]);
+  console.log(obj)
+  
+  };
+  
+  // Call getAverageCost after save
+  CourseSchema.post('save', function() {
+    this.constructor.getAverageCost(this.bootcamp._id);
+  });
+  
+  // Call getAverageCost before remove
+  CourseSchema.pre('remove', function() {
+    this.constructor.getAverageCost(this.bootcamp);
+  });
 
 module.exports = mongoose.model('Course', CourseSchema)
