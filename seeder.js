780be8acc9f363384fecbc1bf66301b8f9e7c052
jiaad <abdulziad08@outlook.com>
2020-03-17 21:28:43 +0100

    Seeder: seed + server.js indent + pathfinder

--- seeder.js
+++ seeder.js
@@ -13,10 +13,11 @@ dotenv.config({path: './config/config.env'})
 const Bootcamp = require('./models/Bootcamp')
 const Course = require('./models/Course')
 const User = require('./models/User')
+const Review = require('./models/Review')
 
 // Connect to DB
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 
 
 // Read JSON files
@@ -30,6 +31,9 @@ const courses = JSON.parse(
 const users = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
 )
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8')
+)
 
 // import Into DB
 
@@ -38,7 +42,8 @@ const importData = async () => {
     await Bootcamp.create(bootcamps)
     await Course.create(courses)
     await User.create(users)
-    console.log('Data Imported...'.green.inverse);
+    await Review.create(reviews)
+    console.log('Data Imported...'.green.inverse)
     process.exit()
   } catch (err) {
     console.log(err)
@@ -50,13 +55,14 @@ const importData = async () => {
 
 const deleteData = async() => {
   try {
-    await Bootcamp.deleteMany();
-    await Course.deleteMany();
+    await Bootcamp.deleteMany()
+    await Course.deleteMany()
     await User.deleteMany()
+    await Review.deleteMany()
     console.log("Data deleted....".red.inverse)
     process.exit()
   } catch (e) {
-    console.log(err);
+    console.log(err)
   }
 }
 
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const fileupload      = require('express-fileupload')
 const cookieParser    = require('cookie-parser')
 const cors            = require("cors");
 const errorHandler    = require('./middleware/error')
-
+var pathfinderUI      = require('pathfinder-ui')
 //LOAD ENV VARS
 // IMPORTANT: toujours mettre le ENV au dessus des import qui ont des ENV
 // On peut ramener en haut mais faut
@@ -40,7 +40,7 @@ if(process.env.NODE_ENV == 'development'){
 const auth        = require('./routes/auth')
 const bootcamps   = require('./routes/bootcamps')
 const courses     = require('./routes/courses')
-
+const review      = require('./routes/reviews')
 // File uploading
 app.use(fileupload())
 
@@ -50,10 +50,15 @@ app.use(express.static(path.join(__dirname, 'public')))
 // MOUNT ROUTERS
 app.use('/api/v1/auth', auth)
 app.use('/api/v1/bootcamps/', bootcamps);
-app.use('/api/v1/courses', courses);
-
+app.use('/api/v1/courses/', courses);
+app.use('/api/v1/reviews/', review)
 // ERROR HANLDER : MUST BE AFTER MOUNT ROUTERS
 app.use(errorHandler);
+
+app.use('/pathfinder', function(req, res, next){
+	pathfinderUI(app)
+	next()
+}, pathfinderUI.router)
 
 // SERVER INITIALIZATION
 const server = app.listen(PORT,
